f8023ecce829e9f5f0b8168895e0d144d02411f5
Romain Pelisse <belaran@gmail.com>
2018-01-25 15:24:29 +0100

    Remove mount of Maven into the container

--- docker-build.sh
+++ docker-build.sh
@@ -2,6 +2,9 @@
 
 readonly WORKSPACE=$(readlink -m  -n "${1}")
 readonly M2_HOME=${M2_HOME:-"${HOME}/.m2"}
+readonly DOCKER_USER=${DOCKER_USER:-'builder'}
+
+readonly BUILD_COMMAND=${2:-'/bin/bash'}
 
 if [ -z "${WORKSPACE}" ]; then
   echo "Provided workspace to mount and build from"
@@ -13,17 +16,11 @@ if [ ! -d "${JAVA_HOME}" ]; then
   exit 2
 fi
 
-if [ ! -d "${MAVEN_HOME}" ]; then
-  echo Invalid MAVEN_HOME:${MAVEN_HOME}
-  exit 3
-fi
-
 readonly DOCKER_IMAGE=${DOCKER_IMAGE:-'builder:latest'}
 
 readonly DOCKER_JAVAZI_MOUNT=${DOCKER_JAVAZI_MOUNT:-'/usr/share/javazi-1.8/:/usr/share/javazi-1.8/:ro'}
 readonly DOCKER_WORKSPACE_MOUNT="${WORKSPACE}:/workspace:rw"
-readonly DOCKER_MAVEN_HOME_MOUNT="${MAVEN_HOME}:/maven:rw"
-readonly DOCKER_MAVEN_M2_HOME_MOUNT="${M2_HOME}:/workspace/.m2:rw"
+readonly DOCKER_MAVEN_M2_HOME_MOUNT="${M2_HOME}:/workspace/.m2/repository:rw"
 readonly DOCKER_JAVA_HOME_MOUNT="${JAVA_HOME}:/java:ro"
 
 if [ "$(getenforce | grep -e Enforcing -c)" -gt 0 ]; then
@@ -31,8 +28,8 @@ if [ "$(getenforce | grep -e Enforcing -c)" -gt 0 ]; then
   exit 4
 fi
 
-docker run --user builder --workdir /workspace -ti \
-          -e MAVEN_HOME='/maven' -e JAVA_HOME='/java' -e LOCAL_REPO_DIR="/workspace/.m2/repository" \
+docker run --user "${DOCKER_USER}" --workdir /workspace -ti \
+           -e JAVA_HOME='/java' -e LOCAL_REPO_DIR="/workspace/.m2/repository" \
            -v "${DOCKER_WORKSPACE_MOUNT}" \
-           -v "${DOCKER_MAVEN_HOME_MOUNT}" -v "${DOCKER_MAVEN_M2_HOME_MOUNT}" \
-           -v "${DOCKER_JAVAZI_MOUNT}" -v "${DOCKER_JAVA_HOME_MOUNT}" "${DOCKER_IMAGE}" '/bin/bash'
+           -v "${DOCKER_MAVEN_M2_HOME_MOUNT}" \
+           -v "${DOCKER_JAVAZI_MOUNT}" -v "${DOCKER_JAVA_HOME_MOUNT}" "${DOCKER_IMAGE}" "${BUILD_COMMAND}"
